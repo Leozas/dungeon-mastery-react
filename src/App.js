250e60d2c7ab7e5a2de3d9657e76c66341ceefe4
Michael Loyselle <52831149+Leozas@users.noreply.github.com>
2019-11-26 11:36:10 -0500

    User-Registeration Update 0.0.2 - Form+Link+Response Implimentation
    Some other forms worked on the side here

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 
 import Register from './components/Register'
+import Login from './components/Login'
+
 
 function App() {
   return (
  <>
   <Register />
+  <Login />
  </>
   );
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,43 +1,83 @@
-import React from 'react';
-import { Button, Form, FormGroup, Label, Input} from 'reactstrap';
+import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input, Container, Row, Col } from 'reactstrap';
+import axios from 'axios'
 
-const Register = (props) => {
-/*
-  axios({
-    method: 'post',
-    url: '/login',
-    data: {
-      firstName: 'Finn',
-      lastName: 'Williams'
-    }
-  });
-  */
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstname: '',
+      lastname: '',
+      username: '',
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  return (
-    <Form>
-      <FormGroup>
-        <Label for="registrationEmail">Email</Label>
-        <Input type="email" name="email" id="registrationEmail" placeholder="hello@emample.com" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="registrationPassword">Password</Label>
-        <Input type="password" name="password" id="registrationPassword" placeholder="Password" maxLength="12"/>
-      </FormGroup>
-      <FormGroup>
-        <Label for="registrationPassword">First Name</Label>
-        <Input type="text" name="firstname" id="registrationFirstName" placeholder="First Name" maxLength="12"/>
-      </FormGroup>
-      <FormGroup>
-        <Label for="registrationPassword">Last Name</Label>
-        <Input type="text" name="lastname" id="registrationPassword" placeholder="Last Name" maxLength="12"/>
-      </FormGroup>
-      <FormGroup>
-        <Label for="registrationPassword">Username</Label>
-        <Input type="text" name="username" id="registrationUsername" placeholder="Username" maxLength="12"/>
-      </FormGroup>
-      <Button>Submit</Button>
-    </Form>
-  );
+
+
+  handleChange(event){
+    this.setState({ 
+      firstname: event.target.value,
+      lastname: event.target.value,
+      username: event.target.value,
+      email: event.target.value,
+      password: event.target.value });
+  };
+
+  handleSubmit(event){
+    event.preventDefault();
+
+    const user = {
+      firstname: this.state.firstname,
+      lastname: this.state.lastname,
+      username: this.state.username,
+      email: this.state.email,
+      password: this.state.password,
+    };
+
+    axios.post('http://127.0.0.1:8000/api/register', user)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      });
+    
+  };
+  render() {
+    return (
+      <Container> Register
+        <Row>
+          <Col>
+            <Form onSubmit={this.handleSubmit}>
+              <FormGroup>
+                <Label for="registrationFirstname">First Name:</Label>
+                <Input type="text" name="firstname" id="registrationFirstName" placeholder="First Name, Maximum 12 Characters" maxLength="12" onChange={this.handleChange} />
+              </FormGroup>
+              <FormGroup>
+                <Label for="registrationLastname">Last Name:</Label>
+                <Input type="text" name="lastname" id="registrationLastname" placeholder="Last Name, Maximum 12 Characters" maxLength="12" onChange={this.handleChange} />
+              </FormGroup>
+              <FormGroup>
+                <Label for="registrationUsername">Username:</Label>
+                <Input type="text" name="username" id="registrationUsername" placeholder="Username, Maximum 12 Characters" maxLength="12" onChange={this.handleChange} />
+              </FormGroup>
+              <FormGroup>
+                <Label for="registrationEmail">Email:</Label>
+                <Input type="email" name="email" id="registrationEmail" placeholder="hello@emample.com" onChange={this.handleChange} />
+              </FormGroup>
+              <FormGroup>
+                <Label for="registrationPassword">Password:</Label>
+                <Input type="password" name="password" id="registrationPassword" placeholder="Password, Maximum 12 Characters" maxLength="12" onChange={this.handleChange} />
+              </FormGroup>
+              <Button type='submit'>Submit</Button>
+            </Form >
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default Register;
